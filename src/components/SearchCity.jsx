0a589117121fe67fd6494d49ce81fcb3bc1d2bf7
KyrieeWen <41510801+KyrieeWen@users.noreply.github.com>
2021-06-02 13:01:09 +1000

    Update SearchCity.jsx

--- src/components/SearchCity.jsx
+++ src/components/SearchCity.jsx
@@ -1,32 +1,30 @@
 import React , {Fragment }from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { CSSTransition } from 'react-transition-group'
-import { SearchBar, SearchInput } from '../css/SearchCity'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { SearchBar, SearchInput, SearchIcon } from '../css/SearchCity'
+// import { connect } from 'react-redux'
 
-const SearchCity = ({ submit, value, focused, change, showResult, inputFocus, inputBlur }) => {
+const SearchCity = (props) => {
+  const {showResult, value, submit, change, map } = props
+
   return (
     <Fragment>
       {/* 默认界面为showResult为false 搜索栏在top：30%，获得数据后搜索栏上升到top：0% */}
       <SearchBar showResult={showResult} onSubmit={submit}>
-        <CSSTransition
-          in={focused}
-          timeout={200}
-          classNames='slide'
-        >
-          <SearchInput 
-            className={focused ? 'focused' : ''}
-            type='text' 
-            value={value} 
-            placeholder='Enter city'                
-            onChange={change}
-            onFocus={inputFocus}
-            onBlur={inputBlur}
-          />
-            {/* <SearchIcon className={focused ? 'focused' : ''}>
-              <FontAwesomeIcon icon={faSearch} />
-            </SearchIcon> */}
-        </CSSTransition>
+        <SearchInput 
+          type='text' 
+          value={value} 
+          placeholder='Enter city'        
+          onChange={change}
+        />
+        <SearchIcon className='faSearch'>
+          <FontAwesomeIcon icon={faSearch} />
+        </SearchIcon>
+        <SearchIcon className='faMapMarkerAlt'>
+          <FontAwesomeIcon 
+            icon={faMapMarkerAlt} 
+            onClick={map} />
+        </SearchIcon>
       </SearchBar>
     </Fragment>
   )
